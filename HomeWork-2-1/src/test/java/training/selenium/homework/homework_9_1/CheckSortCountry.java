39337d46fe09d37c4d2a037741a10cd809413ed4
Anton Titov <272acy@gmail.com>
2018-09-19 10:06:28 +0500

    add b

--- HomeWork-2-1/src/test/java/training/selenium/homework/homework_9_1/CheckSortCountry.java
+++ HomeWork-2-1/src/test/java/training/selenium/homework/homework_9_1/CheckSortCountry.java
@@ -2,6 +2,7 @@ package training.selenium.homework.homework_9_1;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,8 +16,11 @@ import static junit.framework.TestCase.assertEquals;
 public class CheckSortCountry {
 
     private static final String COUNTRIES_PAGE = "http://localhost/litecart/admin/?app=countries&doc=countries";
-    private static final String COUNTRY_NAME = "//tr[@class='row'][%d]/td[%d]";
+    private static final String COUNTRY = "//tr[@class='row'][%d]/td[%d]";
+    private static final String ROW_COUNTRY = ".row";
     private static final int COUNTRY_COL_INDEX = 5;
+    private static final int COL_TIME_ZONE = 6;
+    private static final int COL_INDEX_LINK = 5;
 
 
     private WebDriver driver;
@@ -26,19 +30,19 @@ public class CheckSortCountry {
     @Before
     public void start() {
         driver = new ChromeDriver();
-        driver.manage().window().fullscreen();
+        //driver.manage().window().fullscreen();
+        login();
     }
 
-
+    @Ignore
     @Test
     public void checkAlphabetCountryTest() {
-        login();
         openPage(COUNTRIES_PAGE);
 
         List<String> countryName = new ArrayList<>();
 
-        for (int i = 0; i < countElements(driver, By.cssSelector(".row")); i++) {
-            countryName.add(driver.findElement(By.xpath(String.format(COUNTRY_NAME,i+1,COUNTRY_COL_INDEX))).getAttribute("innerText"));
+        for (int i = 0; i < countElements(driver, By.cssSelector(ROW_COUNTRY)); i++) {
+            countryName.add(getTextElement(driver, COUNTRY,i+1, COUNTRY_COL_INDEX));
         }
 
         List<String> countryNameSort = new ArrayList<>();
@@ -48,7 +52,27 @@ public class CheckSortCountry {
         for (int i = 0; i < countryName.size(); i++) {
             assertEquals(countryNameSort.get(i), countryName.get(i));
         }
+
     }
+
+    @Test
+    public void checkCountryAlphabetTimeZoneTest() {
+        openPage(COUNTRIES_PAGE);
+        List<WebElement> countryWithTimeZone = new ArrayList<>();
+        for (int i = 0; i < countElements(driver, By.cssSelector(ROW_COUNTRY)); i++) {
+            if (Integer.parseInt(getTextElement(driver, COUNTRY, i+1, COL_TIME_ZONE)) > 0 ) {
+                countryWithTimeZone.add(driver.findElement(By.xpath(String.format(COUNTRY, i + 1, COL_INDEX_LINK))));
+                System.out.println(driver.findElement(By.xpath(String.format(COUNTRY, i + 1, COL_INDEX_LINK))).getAttribute("innerHTML"));
+            }
+        }
+
+        for (WebElement element : countryWithTimeZone) {
+            element.findElement(By.cssSelector("a")).click();
+            checkSortTimeZone(driver);
+        }
+
+    }
+
 
 
     @After
@@ -73,5 +97,25 @@ public class CheckSortCountry {
 
     private int countElements(final WebDriver driver, final By locator) {
         return driver.findElements(locator).size();
+    }
+
+    private String getTextElement(WebDriver driver, String xPathLocator, int firstIndex, int secondIndex) {
+       return driver.findElement(By.xpath(String.format(xPathLocator, firstIndex, secondIndex))).getAttribute("innerText");
+    }
+
+    private boolean checkSortTimeZone(WebDriver driver) {
+        ArrayList<String> listTimeZone = new ArrayList<>();
+        List<WebElement> listTimeZoneElements = driver.findElements(By.xpath("//table[@id='table-zones']/*/tr/td[3]"));
+
+        System.out.println(listTimeZoneElements.size());
+        int i = 0;
+        for (WebElement element : listTimeZoneElements) {
+            if (!element.getText().isEmpty()) {
+                listTimeZone.add(element.getText());
+                i++;
+            }
+        }
+        System.out.println(i);
+        return true;
     }
 }
